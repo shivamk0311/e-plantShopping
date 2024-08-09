087c50d7123eefdaa8f102b1e1690b23e26e9049
shivamk0311 <sxk2125@mavs.uta.edu>
2024-08-09 12:19:10 -0400

    Working

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,13 +1,14 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { useDispatch } from 'react-redux';
-import { addItem } from './CartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem, selectTotalItems } from './CartSlice';
 
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const totalItems = useSelector(selectTotalItems);
     const dispatch = useDispatch();
     const plantsArray = [
         {
@@ -258,6 +259,8 @@ const handleContinueShopping = (e) => {
         [plant.name]: true,
     }));
   };
+
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -275,14 +278,14 @@ const handleContinueShopping = (e) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><span style={{fontSize: 30}}>{totalItems}</span></h1></a></div>
             </div>
         </div>
         {!showCart? (
         <div className="product-grid">
             {plantsArray.map((category,index) => (
                 <div key ={index}>
-                    <h1><div>{category.category}</div></h1>
+                    <h1><div className='plant_heading'>{category.category}</div></h1>
                     <div className='product-list'>
                         {category.plants.map((plant,plantIndex)=> (
                             <div className='product-card' id={plantIndex}>
@@ -291,7 +294,7 @@ const handleContinueShopping = (e) => {
                                 <div className='product-title'>{plant.description}</div>
                                 <div className='product-price'>{plant.cost}</div>
                                 <button className='product-button' onClick={()=>handleAddToCart(plant)} disabled={addedToCart[plant.name]} style={{
-                backgroundColor: addedToCart[plant.name] ? 'grey' : '#4CAF50'}}>{addedToCart[plant.name] ? "Added To Cart" : "Add To Cart"}</button>
+                                backgroundColor: addedToCart[plant.name] ? 'grey' : '#4CAF50'}}>{addedToCart[plant.name] ? "Added To Cart" : "Add To Cart"}</button>
                             </div>
                         ))}
                     </div>
